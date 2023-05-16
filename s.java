288163e6a85ae9a7a9f3ea577cde61dc9ad2c281
wangqingxin <778489577@qq.com>
2023-05-16 17:11:09 +0800

    s.java2

--- s.java
+++
@@ -1,8 +0,0 @@
-public class s {
-
-    public static void main(String[] args) {
-
-        System.out.println("7bd3d85645fe0b076567f406fa1533c68d7cdd51".length());
-        System.out.println("你是个人才");
-    }
-}
